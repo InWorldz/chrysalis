5c74e12267a4f4619a61323c5aad12421c37929f
David Daeschler <david.daeschler@gmail.com>
2017-01-21 22:55:37 -0600

    Implementing HTTP frontend

--- src/HttpFrontend.cs
+++ src/HttpFrontend.cs
@@ -1,13 +1,51 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace chrysalis
 {
+    /// <summary>
+    /// Frontend to a simple HTTP server
+    /// </summary>
     class HttpFrontend
     {
-        private HttpListener _httpListener;
-        private bool _stop = false;
+        /// <summary>
+        /// The maximum number of requests allowed in-flight
+        /// </summary>
+        public int MaxConcurrentRequests { get; set; }
 
+        /// <summary>
+        /// Delegate type to handle unhandled exceptions
+        /// </summary>
+        /// <param name="e">The exception that was thrown</param>
+        public delegate void UnhandledExceptionHandler(Exception e);
+
+        /// <summary>
+        /// Registration for delegates to be called in the event of an unhandled exception
+        /// </summary>
+        public event UnhandledExceptionHandler OnUnhandledException;
+
+        /// <summary>
+        /// Handler functions that can respond to requests
+        /// </summary>
+        /// <param name="context">The HTTP context object</param>
+        /// <param name="request">The request object</param>
+        public delegate void RequestHandler(HttpListenerContext context, HttpListenerRequest request);
+
+        private readonly HttpListener _httpListener;
+        private readonly CancellationTokenSource _tokenSource = new CancellationTokenSource();
+
+        /// <summary>
+        /// Collection of handlers based on [HTTP_METHOD, PATH]
+        /// </summary>
+        private Dictionary<Tuple<string, string>, RequestHandler> _handlers = new Dictionary<Tuple<string, string>, RequestHandler>(); 
+
+        /// <summary>
+        /// Constructs a new HttpFrontend
+        /// </summary>
+        /// <param name="prefixes"></param>
         public HttpFrontend(IEnumerable<string> prefixes)
         {
             _httpListener = new HttpListener();
@@ -17,19 +55,57 @@ namespace chrysalis
             }
         }
 
+        /// <summary>
+        /// Starts HTTP services
+        /// </summary>
         public async void Start()
         {
             _httpListener.Start();
 
-            while (!_stop)
+            var requests = new HashSet<Task>();
+            for (int i = 0; i < MaxConcurrentRequests; i++)
             {
-                await _httpListener.GetContextAsync();
+                requests.Add(_httpListener.GetContextAsync());
+            }
+                
+            while (!_tokenSource.Token.IsCancellationRequested)
+            {
+                Task t = await Task.WhenAny(requests);
+                requests.Remove(t);
+
+                if (t is Task<HttpListenerContext>)
+                {
+                    var context = (t as Task<HttpListenerContext>).Result;
+                    requests.Add(ProcessRequestAsync(context));
+                    requests.Add(_httpListener.GetContextAsync());
+                }
+                else
+                {
+                    if (t.IsFaulted)
+                    {
+                        OnUnhandledException?.Invoke(t.Exception);
+                    }
+                }
+                
             }
         }
 
+        /// <summary>
+        /// Processes a single request from the HTTP listener
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        private async Task ProcessRequestAsync(HttpListenerContext context)
+        {
+
+        }
+
+        /// <summary>
+        /// Stops HTTP services
+        /// </summary>
         public void Stop()
         {
-            _stop = true;
+            _tokenSource.Cancel();
         }
 
     }
