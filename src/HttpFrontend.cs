00d4e1394d2a21d673c2a7fdb81b7f43d1c136cf
David Daeschler <david.daeschler@gmail.com>
2017-01-21 21:51:16 -0600

    Start framework

---
+++ src/HttpFrontend.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Net;
+
+namespace chrysalis
+{
+    class HttpFrontend
+    {
+        private HttpListener _httpListener;
+        private bool _stop = false;
+
+        public HttpFrontend(IEnumerable<string> prefixes)
+        {
+            _httpListener = new HttpListener();
+            foreach (var prefix in prefixes)
+            {
+                _httpListener.Prefixes.Add(prefix);
+            }
+        }
+
+        public async void Start()
+        {
+            _httpListener.Start();
+
+            while (!_stop)
+            {
+                await _httpListener.GetContextAsync();
+            }
+        }
+
+        public void Stop()
+        {
+            _stop = true;
+        }
+
+    }
+}
