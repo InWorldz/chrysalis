4577663307e083ab18fd4348e830004a9fd25b8b
David Daeschler <david.daeschler@gmail.com>
2017-03-30 19:35:33 -0500

    Run conversion as a threaded task

--- src/Controllers/GeometryController.cs
+++ src/Controllers/GeometryController.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 using System.Threading.Tasks;
 using FlatBuffers;
 using InWorldz.Arbiter.Serialization;
@@ -25,10 +26,32 @@ namespace InWorldz.Chrysalis.Controllers
             //halcyon gemoetry is coming in as a primitive flatbuffer object
             //as binary in the body. deserialize and convert using the prim exporter
             ByteBuffer body = await StreamUtil.ReadStreamFullyAsync(request.InputStream);
-            var prim = HalcyonPrimitive.GetRootAsHalcyonPrimitive(body);
-            var part = Mapper.MapFlatbufferPrimToPart(prim);
 
-            
+            ExportResult babylonExport = null;
+            Tuple<byte[], int, int> babylonFlatbuffer = null;
+
+            //The actual conversion is CPU intensive. Use a thread task
+            await Task.Run(() =>
+            {
+                var prim = HalcyonPrimitive.GetRootAsHalcyonPrimitive(body);
+                var part = Mapper.MapFlatbufferPrimToPart(prim);
+
+                var displayData = GroupLoader.Instance.ExtractPrimMesh(part, new GroupLoader.LoaderParams(), null);
+                BabylonFlatbufferFormatter formatter = new BabylonFlatbufferFormatter();
+
+                babylonExport = formatter.Export(displayData);
+                babylonFlatbuffer = babylonExport.FaceBlob;
+            });
+
+            context.Response.StatusCode = 200;
+            context.Response.AddHeader("Content-Type", "application/octet-stream");
+            context.Response.AddHeader("Etag", babylonExport.Hash.ToString());
+
+            await context.Response.OutputStream.WriteAsync(babylonFlatbuffer.Item1, babylonFlatbuffer.Item2,
+                babylonFlatbuffer.Item3);
+
+            await context.Response.OutputStream.FlushAsync();
+            context.Response.Close();
         }
 
         private async Task ConvertHalcyonGroupToBabylon(HttpListenerContext context, HttpListenerRequest request)
@@ -37,29 +60,27 @@ namespace InWorldz.Chrysalis.Controllers
             //as binary in the body. deserialize and convert using the prim exporter
             ByteBuffer body = await StreamUtil.ReadStreamFullyAsync(request.InputStream);
 
-            var group = HalcyonGroup.GetRootAsHalcyonGroup(body);
-            var sog = Mapper.MapFlatbufferGroupToSceneObjectGroup(group);
+            ExportResult babylonExport = null;
+            Tuple<byte[], int, int> babylonFlatbuffer = null;
 
-            var displayData = GroupLoader.Instance.GroupDisplayDataFromSOG(UUID.Zero, new GroupLoader.LoaderParams(),
-                sog, null, null, null);
-
-            BabylonFlatbufferFormatter formatter = new BabylonFlatbufferFormatter();
-
-            var babylonExport = formatter.Export(displayData);
-            var babylonFlatbuffer = babylonExport.FaceBlob;
-
-            ObjectHasher hasher = new ObjectHasher();
-
-            //compute the hash for the group to generate the etag
-            ulong hash = 5381;
-            foreach (var obj in babylonExport.BaseObjects)
+            //The actual conversion is CPU intensive. Use a thread task
+            await Task.Run(() =>
             {
-                hash = hasher.GetPrimHash(obj.MaterialHash, obj.ShapeHash);
-            }
+                var group = HalcyonGroup.GetRootAsHalcyonGroup(body);
+                var sog = Mapper.MapFlatbufferGroupToSceneObjectGroup(group);
+
+                var displayData = GroupLoader.Instance.GroupDisplayDataFromSOG(UUID.Zero, new GroupLoader.LoaderParams(),
+                    sog, null, null, null);
+
+                BabylonFlatbufferFormatter formatter = new BabylonFlatbufferFormatter();
+
+                babylonExport = formatter.Export(displayData);
+                babylonFlatbuffer = babylonExport.FaceBlob;
+            });
 
             context.Response.StatusCode = 200;
             context.Response.AddHeader("Content-Type", "application/octet-stream");
-            context.Response.AddHeader("Etag", hash.ToString());
+            context.Response.AddHeader("Etag", babylonExport.Hash.ToString());
 
             await context.Response.OutputStream.WriteAsync(babylonFlatbuffer.Item1, babylonFlatbuffer.Item2,
                 babylonFlatbuffer.Item3);
